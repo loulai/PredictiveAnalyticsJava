87d4ead8cf76828acbecc429864bedfb5170918b
Louise Lai <louiselai88@gmail.com>
2017-10-22 00:51:02 -0400

    the frequencies add up. Print. Screaming.

--- HW2/Homework 2/src/NGram.java
+++ HW2/Homework 2/src/NGram.java
@@ -8,9 +8,9 @@ public class NGram {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hel-fucking-lo");
 		ArrayList<String> test = new ArrayList<String>(Arrays.asList("the theme theme".split(" ")));
-		NGram myNGram = new NGram(test, 2);
+		NGram myNGram = new NGram(test);
+		myNGram.addFrequencies(2);
 		myNGram.printNGram();
 		/*
 		Map<String, Integer> mappy = new HashMap<>();
@@ -27,10 +27,12 @@ public class NGram {
 	public Map<String, ArrayList<Integer>> columnsMap;
 	public int nRow;
 	public int nCol;
+	ArrayList<String> initialTextList;
 	
-	public NGram(ArrayList<String> textList, int n) {
+	public NGram(ArrayList<String> textList) {
+		initialTextList = textList;
 		ArrayList<String> uniqueTextArray = new ArrayList<String>(new LinkedHashSet<String>(textList)); //unique tokens 
-		windowSize = n;
+		
 		rows = uniqueTextArray;
 		nRow = rows.size();
 		nCol = rows.size();
@@ -53,16 +55,39 @@ public class NGram {
 		
 		//rows
 		for(int i = 0; i < nRow; i++) {
-			System.out.printf("%-10s ", rows.get(i));
-			
-			for(String key:columnsMap.keySet()) {
-				System.out.printf("%-10d ", columnsMap.get(key).get(i));
+			System.out.printf("%-10s ", rows.get(i)); //current row (e.g. "the")
+			for(String key:columnsMap.keySet()) { //iterates over all columns for that row
+				System.out.printf("%-10d ", columnsMap.get(key).get(i)); 
 			}
 			System.out.println();
 		}
-		
+	}
 	
+	public NGram addFrequencies(int n) {
+		windowSize = n; 
 		
+		for(int i = 0; i < initialTextList.size() - 1 ;i++) {
+			String currentWord = initialTextList.get(i);
+			String followingWord = initialTextList.get(i+1);
+			int rowPosition = rows.indexOf(currentWord); //position of row in table
+			int currentFreq = columnsMap.get(followingWord).get(rowPosition);
+			int newFreq = currentFreq + 1;
+			
+			//update
+			columnsMap.get(followingWord).set(rowPosition, newFreq);
+		}
+		
+		/*
+		for(int i = 0; i < nRow - 1; i++) {
+			String currentWord = rows.get(i);
+			String followingWord = rows.get(i + 1);
+			
+			int currentFrequency = columnsMap.get(followingWord).get(i);
+			System.out.printf("%-10d ", columnsMap.get(followingWord).set(i, currentFrequency + 1)); 
+		
+		}
+		*/
+		return this;
 	}
 
 }
