c3c903748447109885b71f2d90bbc5cf3bbde1d0
Louise Lai <louiselai88@gmail.com>
2017-10-21 23:33:33 -0400

    the frequencies are printing so pretty I am extra crying

--- HW2/Homework 2/src/NGram.java
+++ HW2/Homework 2/src/NGram.java
@@ -1,15 +1,24 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 public class NGram {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Hel-fucking-lo");
 		ArrayList<String> test = new ArrayList<String>(Arrays.asList("the theme theme".split(" ")));
 		NGram myNGram = new NGram(test, 2);
 		myNGram.printNGram();
+		/*
+		Map<String, Integer> mappy = new HashMap<>();
+		mappy.put("hello", 1);
+		mappy.put("bye", 2);
+		System.out.println(mappy.get("hello"));
+		*/
+		
 	}
 	
 
@@ -28,11 +37,10 @@ public class NGram {
 		
 		Map<String, ArrayList<Integer>> columnsMap = new HashMap<>(); //columns is a hash map
 		for(int i=0; i < nRow; i++) {
-			ArrayList<Integer> freq = new ArrayList<>();
+			ArrayList<Integer> freq = new ArrayList<>(Collections.nCopies(nRow, 0)); //column initialized with all zeros
 			columnsMap.put(uniqueTextArray.get(i), freq);
 		}
 		this.columnsMap = columnsMap;
-		System.out.println(Arrays.asList(columnsMap));
 	}
 	
 	public void printNGram() {
@@ -45,9 +53,15 @@ public class NGram {
 		
 		//rows
 		for(int i = 0; i < nRow; i++) {
-			System.out.printf("%-10s\n", rows.get(i));
+			System.out.printf("%-10s ", rows.get(i));
+			
+			for(String key:columnsMap.keySet()) {
+				System.out.printf("%-10d ", columnsMap.get(key).get(i));
+			}
+			System.out.println();
 		}
 		
+	
 		
 	}
 
--- Homework 2/Homework 2/src/NGram.java
+++
@@ -1,8 +0,0 @@
-
-public class NGram {
-
-	public static void main(String[] args) {
-		System.out.println("Hel-fucking-lo");
-	}
-
-}
